a13c4d35219903443da4ace44a1cb89d918c60f2
emilefrd <92552059+emilefrd@users.noreply.github.com>
2022-06-06 22:36:48 +0200

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-APIKEY = os.environ['APIKEY']
-X = os.environ['LONG']  
-Y = os.environ['LAT']  
+
+APIKEY = os.environ['APIKEY'] #get key 
+X = os.environ['LONG']  #get longitude
+Y = os.environ['LAT']  #get latitude
 
 
 getter = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=" + Y + "&lon=" + X + "&appid=" + APIKEY + "&units=metric")
